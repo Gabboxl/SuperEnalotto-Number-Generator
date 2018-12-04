d29b00123acc5c00722a518877e52c66e9e12ac6
Gabriele <gabboxl0@gmail.com>
2018-12-04 21:53:46 +0100

    fixed grammar

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
 		public static void main(String[] args) {
 			
 			while(true) {
-				String n = null;
+				String n;
 				
 			
 			Scanner reader = new Scanner(System.in);  // Reading from System.in
-			System.out.println("How much numbers do you want? (enter a number or write 'exit' to close the program): ");
+			System.out.println("How many numbers do you want? (enter a number or write 'exit' to close the program): ");
 			 n = reader.nextLine(); // Scans the next token of the input as an int.
 			 
 			 
@@ -29,7 +29,7 @@ public class Main {
 		      System.out.println("Generated : " + randomInt);
 		    }
 			
-			System.out.println("");
+			System.out.println("\n");
 			} catch(Exception e) {
 				
 			}
