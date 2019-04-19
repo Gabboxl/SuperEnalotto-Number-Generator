429b75e77a9dc4bdb3064676f6c96d8a6d4c0fe9
Gabriele <gabboxl0@gmail.com>
2019-04-19 17:19:50 +0200

    cOLOrS11!1!!1
    YaY

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,92 @@ import java.util.Scanner;
 
 public class Main {
 
+	public enum Color {
+		//end string, resets the color
+		RESET("\033[0m"),
+
+		// Regular Colors
+		BLACK("\033[0;30m"),    // BLACK
+		RED("\033[0;31m"),      // RED
+		GREEN("\033[0;32m"),    // GREEN
+		YELLOW("\033[0;33m"),   // YELLOW
+		BLUE("\033[0;34m"),     // BLUE
+		MAGENTA("\033[0;35m"),  // MAGENTA
+		CYAN("\033[0;36m"),     // CYAN
+		WHITE("\033[0;37m"),    // WHITE
+
+		// Bold
+		BLACK_BOLD("\033[1;30m"),   // BLACK
+		RED_BOLD("\033[1;31m"),     // RED
+		GREEN_BOLD("\033[1;32m"),   // GREEN
+		YELLOW_BOLD("\033[1;33m"),  // YELLOW
+		BLUE_BOLD("\033[1;34m"),    // BLUE
+		MAGENTA_BOLD("\033[1;35m"), // MAGENTA
+		CYAN_BOLD("\033[1;36m"),    // CYAN
+		WHITE_BOLD("\033[1;37m"),   // WHITE
+
+		// Underline
+		BLACK_UNDERLINED("\033[4;30m"),     // BLACK
+		RED_UNDERLINED("\033[4;31m"),       // RED
+		GREEN_UNDERLINED("\033[4;32m"),     // GREEN
+		YELLOW_UNDERLINED("\033[4;33m"),    // YELLOW
+		BLUE_UNDERLINED("\033[4;34m"),      // BLUE
+		MAGENTA_UNDERLINED("\033[4;35m"),   // MAGENTA
+		CYAN_UNDERLINED("\033[4;36m"),      // CYAN
+		WHITE_UNDERLINED("\033[4;37m"),     // WHITE
+
+		// Background
+		BLACK_BACKGROUND("\033[40m"),   // BLACK
+		RED_BACKGROUND("\033[41m"),     // RED
+		GREEN_BACKGROUND("\033[42m"),   // GREEN
+		YELLOW_BACKGROUND("\033[43m"),  // YELLOW
+		BLUE_BACKGROUND("\033[44m"),    // BLUE
+		MAGENTA_BACKGROUND("\033[45m"), // MAGENTA
+		CYAN_BACKGROUND("\033[46m"),    // CYAN
+		WHITE_BACKGROUND("\033[47m"),   // WHITE
+
+		// High Intensity
+		BLACK_BRIGHT("\033[0;90m"),     // BLACK
+		RED_BRIGHT("\033[0;91m"),       // RED
+		GREEN_BRIGHT("\033[0;92m"),     // GREEN
+		YELLOW_BRIGHT("\033[0;93m"),    // YELLOW
+		BLUE_BRIGHT("\033[0;94m"),      // BLUE
+		MAGENTA_BRIGHT("\033[0;95m"),   // MAGENTA
+		CYAN_BRIGHT("\033[0;96m"),      // CYAN
+		WHITE_BRIGHT("\033[0;97m"),     // WHITE
+
+		// Bold High Intensity
+		BLACK_BOLD_BRIGHT("\033[1;90m"),    // BLACK
+		RED_BOLD_BRIGHT("\033[1;91m"),      // RED
+		GREEN_BOLD_BRIGHT("\033[1;92m"),    // GREEN
+		YELLOW_BOLD_BRIGHT("\033[1;93m"),   // YELLOW
+		BLUE_BOLD_BRIGHT("\033[1;94m"),     // BLUE
+		MAGENTA_BOLD_BRIGHT("\033[1;95m"),  // MAGENTA
+		CYAN_BOLD_BRIGHT("\033[1;96m"),     // CYAN
+		WHITE_BOLD_BRIGHT("\033[1;97m"),    // WHITE
+
+		// High Intensity backgrounds
+		BLACK_BACKGROUND_BRIGHT("\033[0;100m"),     // BLACK
+		RED_BACKGROUND_BRIGHT("\033[0;101m"),       // RED
+		GREEN_BACKGROUND_BRIGHT("\033[0;102m"),     // GREEN
+		YELLOW_BACKGROUND_BRIGHT("\033[0;103m"),    // YELLOW
+		BLUE_BACKGROUND_BRIGHT("\033[0;104m"),      // BLUE
+		MAGENTA_BACKGROUND_BRIGHT("\033[0;105m"),   // MAGENTA
+		CYAN_BACKGROUND_BRIGHT("\033[0;106m"),      // CYAN
+		WHITE_BACKGROUND_BRIGHT("\033[0;107m");     // WHITE
+
+		private final String code;
+
+		Color(String code) {
+			this.code = code;
+		}
+
+		@Override
+		public String toString() {
+			return code;
+		}
+	}
+
 		public static void main(String[] args) {
 			
 			while(true) {
@@ -10,12 +96,17 @@ public class Main {
 				
 			
 			Scanner reader = new Scanner(System.in);  // Reading from System.in
+			System.out.println(Color.BLUE_BRIGHT);
 			System.out.println("How many numbers do you want? (enter a number or write 'exit' to close the program): ");
+				System.out.println(Color.RESET);
+
 			 n = reader.nextLine(); // Scans the next token of the input as an int.
 			 
 			 
 			 if(n.equals("exit")) {
-			 	System.out.println("\n Thx for using SuperEnalotto Number Generator - ");
+				 System.out.print("\n" + Color.BLACK_BOLD + Color.CYAN_BACKGROUND);
+				 System.out.print("Thx for using SuperEnalotto Number Generator - ");
+				 System.out.println(Color.RESET);
 				 reader.close();
 				 System.exit(0);
 			}
@@ -31,7 +122,9 @@ public class Main {
 			
 			System.out.println("\n");
 			} catch(java.lang.NumberFormatException e) {
-				System.out.println("\n Insert a number thx \n");
+				System.out.print("\n" + Color.BLACK_BOLD + Color.RED_BACKGROUND);
+				System.out.print("Insert a number thx");
+				System.out.println(Color.RESET);
 			}
 			
 		}
